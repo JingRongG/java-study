b4cf44d2f64cf43cb66affb6919234fd17b6fda7
guojr <JingRongG@outlook.com>
2017-07-22 21:33:21 +0800

    添加基础排序算法

--- algorithm/src/com/guojr/demo/Main.java
+++ algorithm/src/com/guojr/demo/Main.java
@@ -2,54 +2,62 @@ package com.guojr.demo;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
 import static java.lang.System.out;
 
 public class Main {
 	private String className  = "com.guojr.demo.Demo01";
 	private String methodName = "SumNum";
 	private Object[] param = new Object[]{100};
-	
+
 	@Test
 	public void test() {
 //		methodName = "SumOddFraction";
 //		param = new Object[]{1.0e-5d};
-		
+
 //		methodName = "ShowAscii";
 //		param = null;
-		
+
 //		methodName = "calcByEnum";
 //		param = new Object[]{300,20,0.8323f};
-		
-		methodName = "PrintChengFaBiao";
-		param = new Object[]{};
-		
+
+//		methodName = "PrintChengFaBiao";
+//		param = new Object[]{};
+
 //		methodName = "PrintPrimes";
 //		param = new Object[]{100};
-		
+
 //		methodName = "queryStrByUnicode";
 //		param = new Object[]{"肏你妈屄"};
+
+//		methodName = "fact";
+//		param = new Object[]{12};
+
+		methodName = "MontePI";
+		param = new Object[]{Integer.MAX_VALUE};
 		try {
-			Main.excute(className,methodName,param);
+			Main.execute(className,methodName,param);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@SuppressWarnings({ "rawtypes", "unused" })
-	public static void excute(String className , String methodName , Object[] param) throws Exception{
+	public static void execute(String className , String methodName , Object[] param) throws Exception{
 		out.printf("class is：%s method is：%s param is：%s \n",className,methodName,param.toString());
 		Class[] pCls = null;
+		Object result;
 		if(param != null){
 			pCls = new Class[param.length];
 			int i = 0;
-			for (Object object : param) 
+			for (Object object : param)
 				pCls[i++] = object.getClass();
 		}
 		//获取类
 		Class<?> cls = Class.forName(className);
 		//获取方法
-		Method method = param == null ? 
+		Method method = param == null ?
 				cls.getDeclaredMethod(methodName) : cls.getDeclaredMethod(methodName,pCls);
 		//生成构造器
 		Constructor<?> construcotr = cls.getConstructor();
@@ -59,9 +67,12 @@ public class Main {
 		Object demo = construcotr.newInstance();
 		//执行方法
 		if(param == null)
-			method.invoke(demo);
+			result =	method.invoke(demo);
 		else
-			method.invoke(demo, param);
+			result =	method.invoke(demo, param);
+
+		if(result != null)
+			out.println(result);
 	}
-	
+
 }
